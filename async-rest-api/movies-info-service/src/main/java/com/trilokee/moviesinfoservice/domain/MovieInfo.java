e8cad545af0a2ca2a03a997ad9e647f21cbc532b
Trilokee <trilokeekumargupta@gmail.com>
2022-10-06 17:01:59 +0530

    Add files via upload
    ok

---
+++ async-rest-api/movies-info-service/src/main/java/com/trilokee/moviesinfoservice/domain/MovieInfo.java
@@ -0,0 +1,13 @@
+package com.trilokee.moviesinfoservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class MovieInfo {
+
+}
